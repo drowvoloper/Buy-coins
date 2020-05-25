6b789be96193daff76d8735ec0b7b25f0ecd14af
Logan <drowvoloper.contact@gmail.com>
2020-05-26 00:42:21 +0200

    functional pagination

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const tablev = document.querySelectorAll(".r3");
 const tablep = document.querySelectorAll(".r4");
 const pagination = document.querySelector(".pagination");
 const pages = [...document.querySelectorAll(".page")];
+const first = document.querySelector(".first");
 const last = document.querySelector(".last");
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
@@ -18,8 +19,10 @@ let columns = [
 ];
 let coins;
 let currentPage = 1;
+let lastPage;
 
-// Step 1: We create a function to fetch the data from the API
+
+// function to fetch the data from the API
 const fetchCoins = async () => {
 	coins = await fetch(
 		'https://api.coinlore.com/api/tickers/'
@@ -27,7 +30,7 @@ const fetchCoins = async () => {
 	).then(data => data.data)
 };
 
-// Step 2: We create the function that will display the data
+// function that will display the data
 const showCoins = async () => {
 	if (!coins) {
 		await fetchCoins();
@@ -35,11 +38,10 @@ const showCoins = async () => {
 
 	// we calculate the first and the last item from coins we need to show
 	// based on the current page
-	//currentPage = 5;
 	let maxCoin = (5 * currentPage) - 1;
 	let minCoin = maxCoin - 4;
 
-	// then we loop through the rows array
+	// then we loop through the columns array
 	for (let i = 0; i <= columns.length; i++) {
 		let coin = coins[minCoin + i];
 		let price = Math.round(coin.price_usd);
@@ -50,39 +52,88 @@ const showCoins = async () => {
 		columns[3][i].innerText = coin.tsupply;
 	}
 
-	createPagination(minCoin, maxCoin);
-
-	//console.log(coins[minCoin]);
-	
+	lastPage = Math.ceil(coins.length / 5); // 5 is the number of coins displayed in each page
+	last.innerText = lastPage;
 };
 
-// We use this function to calculate and display how many 
-// pages will have
-const createPagination = (min, max) => {
-	const numberOfPages = Math.ceil(coins.length / 5); // 5 is the number of coins displayed in each page
-	if (min == 0) {
+// function to show the number of pages in the pagination
+const displayPages = (calculation) => {
+	pages.forEach((page) => {
+		page.classList.remove('current');
+		if (!page.classList.contains('first') &&
+				!page.classList.contains('last')) {
+			let lastValue = parseInt(page.innerText);
+			page.innerText = calculation == '+' ?
+				lastValue + 4 :
+				lastValue - 4;
+		}
+	});
+
+	if (calculation == '+') {
+		currentPage = parseInt(pages[1].innerText);
+	}
+	else {
+		currentPage = pages[4].innerText - 3;
+	}
+
+	pages[1].classList.add('current');
+};
+
+// function to show or hide the "prev" and "next" links
+const prevNext = () => {
+	
+	if (currentPage == 1 || currentPage == 2) {
 		prev.style.display = "none";
 	} else {
 		prev.style.display = "inline";
-		prevPage = min - 2;
 	}
 
-	if (max == coins.length - 1) {
+	if (currentPage == lastPage || currentPage == lastPage - 1) {
 		next.style.display = "none";
 	} else {
 		next.style.display = "inline";
-		nextPage = max + 2;
 	}
+}
 
-	console.log(nextPage);
-
-	pages.forEach((page) => {
-		
-	});
-
-	last.innerText = numberOfPages;
+// We use this function to calculate and display how many 
+// pages will have
+const createPagination = (calculation) => {
+	displayPages(calculation);
+	prevNext();
 };
 
 showCoins();
 
-//console.log(columns[1][0].innerText);
+
+/* Event Listeners */
+pages.forEach((page) => {
+	page.addEventListener('click', (event) => {
+		let value = event.target.innerText;
+		currentPage = parseInt(value);
+		
+		pages.forEach((page) => {
+			page.classList.remove('current');
+		});
+		page.classList.add('current');
+
+		showCoins();
+	});
+});
+prev.addEventListener('click', () => createPagination('-'));
+next.addEventListener('click', () => createPagination('+'));
+first.addEventListener('click', () => {
+	
+	for (let x = 1; x < 5; x++) {
+		pages[x].innerText = x + 1;
+	}
+
+	prevNext();
+});
+
+last.addEventListener('click', () => {
+	for (let x = 1; x < 5; x++) {
+		pages[x].innerText = lastPage - 5 + x;
+	}	
+
+	prevNext();
+});
